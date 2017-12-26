1ada87c45e7fc32032a2e6d1efdbf1639b27c86d
unknown <salekuma@DIN55001727.corp.capgemini.com>
2017-12-26 18:00:32 +0530

    Signed-off-by: unknown <salekuma@DIN55001727.corp.capgemini.com>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'pm-app',
+    selector:'pm-root',
     template:`
             <div>
-            <h2>Hello,world</h2>
-            </div>
-            `
+          <h2>{{pageTitle}}</h2>
+          <pm-products></pm-products></div>
+           `,
+      moduleId: module.id
     }
 
 )
-export class AppComponent{}
+export class AppComponent{
+  //name: String ="Capgemini"
+pageTitle:string='Acme Product Management';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@
 import { NgModule } from "@angular/core"
 import {BrowserModule} from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/app.productList.component";
 
 @NgModule({
 imports:[BrowserModule],
-declarations:[AppComponent],
+declarations:[AppComponent,ProductListComponent],
 bootstrap:[AppComponent]
 
 })
+
 
 export class AppModule{
 
