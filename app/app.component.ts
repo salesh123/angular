c1bcf371217f359f4ef84f8154ca32fe73a57c11
unknown <salekuma@DIN55001727.corp.capgemini.com>
2017-12-26 15:47:17 +0530

    Created Hello World Application

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'pm-app',
+    template:`
+            <div>
+            <h2>Hello,world</h2>
+            </div>
+            `
+    }
+
+)
+export class AppComponent{}
