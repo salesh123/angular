fe257b6db592362dec763545621db5d50370c745
unknown <salekuma@DIN55001727.corp.capgemini.com>
2017-12-27 14:39:48 +0530

    New Changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 
 import { NgModule } from "@angular/core"
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/app.productList.component";
 
 @NgModule({
-imports:[BrowserModule],
+imports:[BrowserModule,FormsModule],
 declarations:[AppComponent,ProductListComponent],
 bootstrap:[AppComponent]
 
--- app/products/app.productList.component.ts
+++ app/products/app.productList.component.ts
@@ -1,4 +1,5 @@
 import{ Component }from'@angular/core';
+import { IProduct } from "./product";
 
 @Component({
     selector:'pm-products',
@@ -6,10 +7,29 @@ import{ Component }from'@angular/core';
     moduleId: module.id
 })
 
-export class ProductListComponent
+export class ProductListComponent 
 {
+    constructor() {
+        this.filterdProduct = this.products;
+    }
     pageTitle:string='Product List';
-    products: any[] = [
+    imageWidth:number=50;
+    imageMargin:number=2;
+    showImage:boolean;
+
+    filterdProduct:any[];
+
+    _listFilter:string;
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter=value;
+        this.performFilter(value);
+        this.filterdProduct = this.performFilter(value); 
+        console.log(value)
+    }
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -18,7 +38,7 @@ export class ProductListComponent
             "description": "15 gallon capacity rolling garden cart",
             "price": 32.99,
             "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            "imageUrl": "https://images.pexels.com/photos/36764/marguerite-daisy-beautiful-beauty.jpg?h=350&auto=compress&cs=tinysrgb"
         },
         {
             "productId": 5,
@@ -28,7 +48,16 @@ export class ProductListComponent
             "description": "Curved claw steel hammer",
             "price": 8.9,
             "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+            "imageUrl": "https://static.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg"
         }
     ];
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }	
+    toggleImage():void{
+    this.showImage=!this.showImage;
+    }
+
 }
